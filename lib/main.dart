5f594fa31aa93caaf7a80876dcde25bb9652f3f1
hikobe8 <hikobe8@163.com>
2019-05-22 19:32:51 +0800

    Add a Stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,30 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Welcome to Flutter',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Welcome to Flutter'),
+          ),
+          body: Center(
+            child: RandomWords(),
+          ),
+        ));
+  }
+}
 
+class RandomWordsState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text(wordPair.asPascalCase),
-        ),
-      )
-    );
+    return Text(wordPair.asPascalCase);
   }
+}
 
+class RandomWords extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => RandomWordsState();
 }
